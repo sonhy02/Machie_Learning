af1741556642aa460ad6516adfd23ff30787b456
sonhy02 <tls1234568@naver.com>
2024-05-31 02:44:36 +0900

    Changes

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = "https://en.wikipedia.org/wiki/"
-r = requests.get(URL)
-
-soup = BeautifulSoup(r.text, "html.parser")
-print(soup.prettify())
+print("Hello World")
